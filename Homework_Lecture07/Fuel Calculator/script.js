d832ab5c2e8f932d7bda4dd5dc68471fc971501c
Nikola Todorovski <45461687+nTodorovski@users.noreply.github.com>
2018-12-10 12:27:03 +0100

    Update script.js

--- Homework_Lecture07/Fuel Calculator/script.js
+++ Homework_Lecture07/Fuel Calculator/script.js
@@ -1,34 +1,53 @@
 let car = {
-    model: "Chevrolet Cruze Sedan",
-    color: "Red",
-    year: 2010,
-    fuel: "Petrol",
-    fuelConsumption: 9,
+    model: "model",
+    color: "color",
+    year: "year",
+    fuel: "fuel",
+    fuelConsumption: "fuelConsumption",
     howMuch: function (km) {
         let liters = (km / 100) * this.fuelConsumption;
         return liters;
     }
 }
-let para1 = document.getElementById("para1");
-para1.innerHTML = `Car: ${car.model}<br />Color: ${car.color}<br />Year: ${car.year}<br />Fuel: ${car.fuel}<br />Fuel Consumption: ${car.fuelConsumption}l/100km`;
-para1.style.fontSize = "x-large";
 let button = document.getElementById("button1");
 button.addEventListener("click",function howMuch(){
+    let model = document.getElementById("1").value;
+    car.model = model;
+    let color = document.getElementById("2").value;
+    car.color = color;
+    let year = document.getElementById("3").value;
+    car.year = year;
+    let fuel = document.getElementById("4").value;
+    car.fuel = fuel;
+    let fuelConsumption = document.getElementById("5").value;
+    car.fuelConsumption = fuelConsumption;
+    let input1 = document.getElementById("input1");
     let value = Number(input1.value);
-    if(document.getElementById("para")){
-        para.innerHTML = "";
-    }
-    if(Number.isNaN(value)){
+    let year1 = Number(year);
+    let fuelConsumption1 = Number(fuelConsumption);
+    if(Number.isNaN(fuelConsumption1)){
         let para = document.createElement("p");
         para.id = "para";
         let body = document.getElementsByTagName("body")[0];
-        para.innerHTML = "Please enter number.";
+        para.innerHTML = "Please enter a number for the fuel consumption.";
+        body.appendChild(para); 
+    } else if(Number.isNaN(year1)){
+        let para = document.createElement("p");
+        para.id = "para";
+        let body = document.getElementsByTagName("body")[0];
+        para.innerHTML = "Please enter a number for the year of the car.";
         body.appendChild(para);
-    } else if(input1.value === null || input1.value === ""){
+    } else if(Number.isNaN(value)){
         let para = document.createElement("p");
         para.id = "para";
         let body = document.getElementsByTagName("body")[0];
-        para.innerHTML = "You didn't enter a value.";
+        para.innerHTML = "Please enter number for the distance you want to pass.";
+        body.appendChild(para);
+    } else if(input1.value === null || input1.value === "" || model === null || model === "" || year === null || year === "" || fuel === null || fuel === "" || fuelConsumption === null || fuelConsumption === ""){
+        let para = document.createElement("p");
+        para.id = "para";
+        let body = document.getElementsByTagName("body")[0];
+        para.innerHTML = "You didn't enter a value in all of the fields.";
         body.appendChild(para);
     } else{
         let input = document.getElementById("input1").value;
